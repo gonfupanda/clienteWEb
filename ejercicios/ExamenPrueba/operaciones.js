9264bedfbc11f6e75c9dd8b0d8f6abdc5faaf6f6
gonfupanda <90763456+gonfupanda@users.noreply.github.com>
2021-10-25 13:39:20 +0200

    Add files via upload

---
+++ ejercicios/ExamenPrueba/operaciones.js
@@ -0,0 +1,4 @@
+function guardarNombreLocalStorage() {//funcion en la que introduzco un dato en la local storage
+    localStorage.setItem("nombre",document.getElementById("dato").value);//setItem para introducir
+
+}
