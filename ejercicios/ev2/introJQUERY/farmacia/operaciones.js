a1e80fc66934f18634a1532861a083215679826d
gonfupanda <90763456+gonfupanda@users.noreply.github.com>
2022-01-17 13:20:36 +0100

    Add files via upload

---
+++ ejercicios/ev2/introJQUERY/farmacia/operaciones.js
@@ -0,0 +1,13 @@
+
+usuarios=[{id:"AAA1111",nombre:"Mikel"},
+{id:"BBB2222",nombre:"Gonzalo"},{id:"CCC3333",nombre:"Ivan"},{id:"DDD4444",nombre:"EvilRaysha"}];
+
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
+$(".operacion").hide(0);
+
+let producto ={//Objeto de medicamentos
+    nombre:nombree,
+    precio :precioo,
+    cantidad:cant
+};
